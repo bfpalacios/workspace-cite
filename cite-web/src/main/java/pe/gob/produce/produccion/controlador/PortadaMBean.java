a2bae9d68d908a8423c81bcb4578736e46a9de7e
Angel <Angel@WINDOWS-SMFT2I7>
2016-05-30 16:36:53 -0500

    portada

--- cite-web/src/main/java/pe/gob/produce/produccion/controlador/PortadaMBean.java
+++ cite-web/src/main/java/pe/gob/produce/produccion/controlador/PortadaMBean.java
@@ -3,15 +3,25 @@ package pe.gob.produce.produccion.controlador;
 
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import pe.gob.produce.cite.bo.UsuarioBO;
+import pe.gob.produce.produccion.model.PortadaModel;
+import pe.gob.produce.produccion.model.ServicioModel;
 
 @Controller("portadaMBean")
 @ViewScoped
 public class PortadaMBean {
+	
+	//@Autowired
+	//private PortadaModel portadaModel;
 
 	//constructor
 		public PortadaMBean(){
 			System.out.println("::::: LOADING PortadaMBean ::::::::");
+			//portadaModel = new PortadaModel();
 		}
 		
 		public String cargarBienvenida(){
@@ -29,4 +39,6 @@ public class PortadaMBean {
 		public String cargarPublicacionesInformativas(){
 			return "/paginas/ModuloProduccion/cite/portadaPrincipal/publicacionesInformativas.xhtml";
 		}
+
+				
 }

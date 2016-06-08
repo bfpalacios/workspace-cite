65af256a71c1dac2c62b2b6e9c458bba06441efe
bfpalacios <bfpalacios@gmail.com>
2016-06-08 07:07:29 -0500

    Se corrige la ruta de las carpetas.

--- cite-web/src/main/java/pe/gob/produce/produccion/controlador/Constantes.java
+++ cite-web/src/main/java/pe/gob/produce/produccion/controlador/Constantes.java
@@ -4,10 +4,10 @@ public class Constantes {
 
 	//public static String url="D:\\DocumentosEveris\\DocumentosEveris\\Requerimientos\\https site administrativo\\";
 	public static String url="C:\\ITP\\DOCUMENTOS_CITE\\MANUALES\\";
-	public static String urlFormatos="C:\\ITP\\DOCUMENTOS_CITE\\MANUALES\\FORMATOS\\";
-	public static String urlProcedimientos="C:\\ITP\\DOCUMENTOS_CITE\\MANUALES\\PROCEDIMIENTOS\\";
-	public static String urlInstructivos="C:\\ITP\\DOCUMENTOS_CITE\\MANUALES\\INSTRUCTIVOS\\";
-	public static String urlDocumentos="C:\\ITP\\DOCUMENTOS_CITE\\MANUALES\\DOCUMENTOS\\";
+	public static String urlFormatos="C:\\ITP\\DOCUMENTOS_CITE\\FORMATOS\\";
+	public static String urlProcedimientos="C:\\ITP\\DOCUMENTOS_CITE\\PROCEDIMIENTOS\\";
+	public static String urlInstructivos="C:\\ITP\\DOCUMENTOS_CITE\\INSTRUCTIVOS\\";
+	public static String urlDocumentos="C:\\ITP\\DOCUMENTOS_CITE\\DOCUMENTOS\\";
 	    
    
     
